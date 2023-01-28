b1f6fe1b98077cd27c179eaedb8f78537b54f8d1
Ulysses Milton <ulysses.github@protonmail.com>
2023-01-28 17:03:30 -0500

    Started Java Game

---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D RPG");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack(); // Causes this window to be sized to fit the preferred size and layouts of its subcomponents
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
