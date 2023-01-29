088d69669e1d24636e5be0688318d10bf209cedf
umilton-git <ulysses.github@protonmail.com>
2023-01-29 17:17:14 -0500

    Created test sprite and added to player entity

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,6 +1,9 @@
 package Entity;
 
+import java.awt.image.BufferedImage;
 public class Entity {
     public int x, y;
     public int speed;
+    public BufferedImage playerPholder; // Contains buffered image data
+    public String direction;
 }
--- src/Entity/Player.java
+++ src/Entity/Player.java
@@ -3,7 +3,10 @@ package Entity;
 import main.GamePanel;
 import main.KeyHandler;
 
-import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.io.IOException;
 
 public class Player extends Entity {
     GamePanel gp;
@@ -14,33 +17,49 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
     public void setDefaultValues() {
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage() {
+        try {
+            playerPholder = ImageIO.read(getClass().getResourceAsStream("/player/LilDude.png"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void update() {
         if(keyH.upPressed == true) {
+            direction = "up";
             y -= speed;
         }
 
         else if(keyH.downPressed == true) {
+            direction = "down";
             y += speed;
         }
 
         else if(keyH.leftPressed == true) {
+            direction = "left";
             x -= speed;
         }
 
         else if(keyH.rightPressed == true) {
+            direction = "right";
             x += speed;
         }
     }
 
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        // g2.setColor(Color.white);
+        // g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = playerPholder;
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
