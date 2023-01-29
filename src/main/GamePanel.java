a77801415d978221fcd4f67fa1285d5da5d6cc2e
umilton-git <ulysses.github@protonmail.com>
2023-01-29 16:23:04 -0500

    Added Entity System

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,12 +7,14 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import Entity.Player;
+
 public class GamePanel extends JPanel implements Runnable{
     // Screen Settings
     final int originalTileSize = 16; // 16x16 Tile Size
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 48x48 tile
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels
@@ -21,6 +23,7 @@ public class GamePanel extends JPanel implements Runnable{
     int FPS = 60;
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     // Set player's default position
     int playerX = 100;
@@ -107,30 +110,14 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void update() {
-        if(keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        }
-
-        else if(keyH.downPressed == true) {
-            playerY += playerSpeed;
-        }
-
-        else if(keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        }
-
-        else if(keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D)g;
-
-        g2.setColor(Color.white);
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
         g2.dispose();
     }
 }
